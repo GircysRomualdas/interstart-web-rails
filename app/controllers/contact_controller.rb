f98e77b073aa92fac7a359a544c64722a258388b
Romualdas <r2udis@gmail.com>
2024-01-10 10:53:00 +0200

    add contact form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,13 @@
 class ContactController < ApplicationController 
     def index 
     end
+
+    def create 
+        puts "|+|------------------------|+|"
+        puts params[:email]
+        puts params[:message]
+        puts "|+|------------------------|+|"
+
+        redirect_to request.referrer || root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get "about", to: "about#index", as: :about
 
   get "contact", to: "contact#index", as: :contact
+  post "contact", to: "contact#create"
 end
