87c3505ee71ce8b77af565fade1c9c1c27edf449
Romualdas <r2udis@gmail.com>
2024-01-11 17:53:42 +0200

    fix login css and pagy css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
-if Comment.count < 100 
-    100.times do |index| 
+if Comment.count < 200 
+    200.times do |index| 
         comment = Comment.create(
             email: "rudis@gmail.com",
             message: "Nr. #{index + 1} Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
         )
 
-        number = rand(0...4) 
+        number = rand(0...3) 
 
         number.times do |indexNumber| 
             Reply.create(
